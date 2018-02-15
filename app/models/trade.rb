97f1fbd5184cf1022896dc7a2a4298dc6aa555a0
rh24 <ms.rebeccahong@gmail.com>
2018-02-15 15:37:53 -0500

    Add slug and class find_by_slug method to User class

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -2,5 +2,6 @@ class Trade < ActiveRecord::Base
   belongs_to :user
 
   def net_profitloss_fiat
+    
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ActiveRecord::Base
   has_many :trades
   has_many :user_years
   has_many :years, through: :user_years
+
+  def slug
+    slugged = self.username.downcase.gsub(/[^0-9A-Za-z]/, '-')
+    slugged
+  end
+
+  def self.find_by_slug(slug)
+    self.all.detect do |user|
+      user if user.slug == slug
+    end
+  end
 end
