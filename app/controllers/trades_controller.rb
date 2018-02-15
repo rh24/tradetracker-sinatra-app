f4f04d503e48f0eebb0e6a3331b303789b94865f
rh24 <ms.rebeccahong@gmail.com>
2018-02-15 16:45:09 -0500

    Create sign up form for new users

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -1,5 +1,13 @@
 class TradesController < ApplicationController
 
+  get '/trades' do
+    if logged_in?
+      erb :'/trades/index'
+    else
+      redirect to '/login'
+    end
+  end
+
   get '/trades/new' do
     if logged_in?
       erb :'/trades/new'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,15 @@
 class UsersController < ApplicationController
 
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'/users/show'
+  end
+  
   get '/signup' do
-    
+    if !logged_in?
+      erb :signup
+    else
+      redirect to '/trades'
+    end
   end
 end
