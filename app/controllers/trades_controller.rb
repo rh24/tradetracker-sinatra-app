b63fc424a37365edfd6056d59b27f9b012efd50f
rh24 <ms.rebeccahong@gmail.com>
2018-02-16 13:59:09 -0500

    Add new trades form and trades/index.erb

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -2,11 +2,17 @@ class TradesController < ApplicationController
 
   get '/trades' do
     @user = current_user
+    @trades = Trade.all
     if logged_in?
       erb :'/trades/index'
     else
       redirect to '/login'
     end
+  end
+
+  post '/trades' do
+    @trade = Trade.create(params)
+    redirect to '/trades'
   end
 
   get '/trades/new' do
