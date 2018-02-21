bf383942aaf637a28e63eca000a2481291a1c8b0
rh24 <ms.rebeccahong@gmail.com>
2018-02-20 19:10:15 -0500

    Add erb :'/years/show' to get '/years/:year' controller action to fix bytesize error

--- app/controllers/years_controller.rb
+++ app/controllers/years_controller.rb
@@ -12,8 +12,10 @@ class YearsController < ApplicationController
   get '/years/:year' do
     @year = Year.find_by(year: params[:year])
     if logged_in? && !!@year
-      @trades = Trade.all.select { |t| t.date.include?(@year.year.to_s) }
+      # @trades = Trade.all.select { |t| t.date.include?(@year.year.to_s) }
+      @trades = Trade.all
       # binding.pry
+      erb :'/years/show'
     elsif !logged_in?
       redirect to '/login'
     else
