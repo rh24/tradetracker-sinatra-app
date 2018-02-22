afc1a8509f423c5487d4d2e433c43f8213fa5cb2
rh24 <ms.rebeccahong@gmail.com>
2018-02-22 14:32:23 -0500

    Fix years/index and change get '/years' controller action to only index years with trades

--- app/controllers/years_controller.rb
+++ app/controllers/years_controller.rb
@@ -2,6 +2,15 @@ class YearsController < ApplicationController
   register Sinatra::Flash
 
   get '/years' do
+    years_with_trades =
+      Trade.all.collect do |t|
+        t.date[0..4].to_i
+      end
+    @years =
+      years_with_trades.each do |y|
+        Year.find_or_create_by(year: y)
+      end
+
     if logged_in?
       erb :'/years/index'
     else
